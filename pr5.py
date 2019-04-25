f8c818344fedb0dc3094b063e5f074f1a6a9e69f
Kim Roche <kimberly.e.roche@gmail.com>
2019-04-25 13:54:27 -0400

    Local changes to pr5

--- pr5.py
+++ pr5.py
@@ -1,9 +1,9 @@
 from shared_imports import *
 
 # testing
-subsample_no = 100
+subsample_no = 5000
 batch_size = 10
-epochs = 10
+epochs = 100
 
 # full
 #subsample_no = 5000
@@ -30,29 +30,30 @@ time_CNN = 0.0 # total runtime (CNN)
 loo = LeaveOneOut()
 loo.get_n_splits(X_GP)
 it = 1
+iter_limit = 100
 for train_index, test_index in loo.split(X_GP):
-    #if it % 10 == 0:
-    print("Evaluating split #" + str(it))
-    it += 1
-    if method == "gpr":
-        X_train, X_test = X_GP[train_index], X_GP[test_index]
+    if it <= iter_limit:
+        print("Evaluating split #" + str(it))
+        it += 1
         y_train, y_test = y[train_index], y[test_index]
-        start = time.time()
-        y_pred = predict_GP(X_train, y_train, X_test)
-        end = time.time()
-        time_GP += end - start
-        error_GP += one_hot_loss(y_test[0], y_pred[0])
-    else:
-        X_train, X_test = X_CNN[train_index], X_CNN[test_index]
-        start = time.time()
-        y_pred = predict_CNN_classification(X_train, y_train, X_test, batch_size=batch_size, epochs=epochs)
-        end = time.time()
-        time_CNN += end - start
-        error_CNN += one_hot_loss(y_test[0], y_pred.tolist())
+        if method == "GPR":
+            X_train, X_test = X_GP[train_index], X_GP[test_index]
+            start = time.time()
+            y_pred = predict_GP(X_train, y_train, X_test)
+            end = time.time()
+            time_GP += end - start
+            error_GP += one_hot_loss(y_test[0], y_pred[0])
+        else:
+            X_train, X_test = X_CNN[train_index], X_CNN[test_index]
+            start = time.time()
+            y_pred = predict_CNN_classification(X_train, y_train, X_test, batch_size=batch_size, epochs=epochs, verbose=1)
+            end = time.time()
+            time_CNN += end - start
+            error_CNN += one_hot_loss(y_test[0], y_pred.tolist())
 
-if method == "gpr":
-    print("Misclassification percent (GP): " + str(error_GP/no_samples))
-    print("Average runtime (GP): " + str(round(time_GP/no_samples, round_tol)) + " sec")
+if method == "GPR":
+    print("Misclassification percent (GP): " + str(error_GP/iter_limit))
+    print("Average runtime (GP): " + str(round(time_GP/iter_limit, round_tol)) + " sec")
 else:
-    print("Misclassification percent (CNN): " + str(error_CNN/no_samples))
-    print("Average runtime (CNN): " + str(round(time_CNN/no_samples, round_tol)) + " sec")
+    print("Misclassification percent (CNN): " + str(error_CNN/iter_limit))
+    print("Average runtime (CNN): " + str(round(time_CNN/iter_limit, round_tol)) + " sec")
