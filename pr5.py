c39ebbc0a344b890e8f9e5ba162a594945ee4542
Kimberly Roche <ker48@c1-01-1.genome.duke.edu>
2019-04-25 13:41:14 -0400

    HARDAC changes to pr5

--- pr5.py
+++ pr5.py
@@ -1,19 +1,26 @@
 from shared_imports import *
 
 # testing
-#subsample_no = 100
-#batch_size = 10
-#epochs = 10
+subsample_no = 100
+batch_size = 10
+epochs = 10
 
 # full
-subsample_no = 1000 # prev job running at 5K
-batch_size = 128
-epochs = 100
+#subsample_no = 5000
+#batch_size = 128
+#epochs = 100
+
+# get similarity measure from user
+method = "cnn"
+if(len(sys.argv) > 1):
+    if sys.argv[1] == "gpr":
+        method = "GPR"
 
 (X_GP, X_CNN, y) = load_cifar10(subsample_no=subsample_no)
 
 no_samples = X_GP.shape[0]
 
+print("Method: " + method)
 print("Subsampling to " + str(subsample_no))
 
 error_GP = 0.0 # sum of misclassifications (GP)
@@ -27,22 +34,25 @@ for train_index, test_index in loo.split(X_GP):
     #if it % 10 == 0:
     print("Evaluating split #" + str(it))
     it += 1
-    X_train, X_test = X_GP[train_index], X_GP[test_index]
-    y_train, y_test = y[train_index], y[test_index]
-    start = time.time()
-    #y_pred = predict_GP(X_train, y_train, X_test)
-    end = time.time()
-    time_GP += end - start
-    #error_GP += one_hot_loss(y_test[0], y_pred[0])
-    X_train, X_test = X_CNN[train_index], X_CNN[test_index]
-    start = time.time()
-    y_pred = predict_CNN_classification(X_train, y_train, X_test, batch_size=batch_size, epochs=epochs)
-    end = time.time()
-    time_CNN += end - start
-    error_CNN += one_hot_loss(y_test[0], y_pred.tolist())
+    if method == "gpr":
+        X_train, X_test = X_GP[train_index], X_GP[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        start = time.time()
+        y_pred = predict_GP(X_train, y_train, X_test)
+        end = time.time()
+        time_GP += end - start
+        error_GP += one_hot_loss(y_test[0], y_pred[0])
+    else:
+        X_train, X_test = X_CNN[train_index], X_CNN[test_index]
+        start = time.time()
+        y_pred = predict_CNN_classification(X_train, y_train, X_test, batch_size=batch_size, epochs=epochs)
+        end = time.time()
+        time_CNN += end - start
+        error_CNN += one_hot_loss(y_test[0], y_pred.tolist())
 
-print("Misclassification percent (GP): " + str(error_GP/no_samples))
-print("Average runtime (GP): " + str(round(time_GP/no_samples, round_tol)) + " sec")
-
-print("Misclassification percent (CNN): " + str(error_CNN/no_samples))
-print("Average runtime (CNN): " + str(round(time_CNN/no_samples, round_tol)) + " sec")
+if method == "gpr":
+    print("Misclassification percent (GP): " + str(error_GP/no_samples))
+    print("Average runtime (GP): " + str(round(time_GP/no_samples, round_tol)) + " sec")
+else:
+    print("Misclassification percent (CNN): " + str(error_CNN/no_samples))
+    print("Average runtime (CNN): " + str(round(time_CNN/no_samples, round_tol)) + " sec")
